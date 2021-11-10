6d7ff2892874a5d3b5ca1652c9a6298b9f2512a3
Junaid Girkar <junaidgirkar@gmail.com>
2021-11-10 18:08:17 +0000

    From deployment

--- SHMT/settings.py
+++ SHMT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7wv!4-tuy)_34$x3v5e)c0i(h49rj8*8cv8smqpelf0j6mjlk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
